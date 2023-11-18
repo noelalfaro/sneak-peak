acf8e9a616ebb0b23e7bd1a910d3afe338fc7a53
Noel <ndalfaro333@gmail.com>
2023-11-18 00:33:40 -0600

    added api url to github login component

--- client/src/components/GithubLogin.jsx
+++ client/src/components/GithubLogin.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function GithubLogin() {
 	const API_URL =
 		process.env.NODE_ENV === "production"
-			? "sneak-peak-server.up.railway.app"
+			? "https://sneak-peak-server.up.railway.app"
 			: "http://localhost:3001";
 	const handleLogin = () => {
 		window.location.href = `${API_URL}/auth/github`;
